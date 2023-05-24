826a740991ea20dd72089f6562041c2d8269514d
Jonathan <jonathandagan12@gmail.com>
2023-05-24 03:07:27 +0300

    added jwt experation

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -1,5 +1,6 @@
 export const ENV = {
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1d',
     PORT: process.env.PORT || 5000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/ktrust'
