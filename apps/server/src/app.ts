c1bd8f7a79a7035054b2df4806d13c6488307a2b
Jonathan <jonathandagan12@gmail.com>
2023-05-24 03:07:48 +0300

    added auth routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,16 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 // Routes
-app.use("/api/users", userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+});  
 
 export default app;
