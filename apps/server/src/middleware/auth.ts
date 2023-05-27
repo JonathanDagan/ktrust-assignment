711536843232d047353f863ba0e0727513f50d53
Jonathan <jonathandagan12@gmail.com>
2023-05-27 23:48:44 +0300

    formatting

--- apps/server/src/middleware/auth.ts
+++ apps/server/src/middleware/auth.ts
@@ -1,24 +1,21 @@
-import { ENV } from '../config/env';
+import { ENV } from "../config/env";
 
-import {expressjwt} from 'express-jwt';
-import { Request, Response, NextFunction } from 'express';
-import { SigningAlgorithm } from '../utils/consts';
-
+import { expressjwt } from "express-jwt";
+import { Request, Response, NextFunction } from "express";
+import { SigningAlgorithm } from "../utils/consts";
 
 export const jwtCheck = expressjwt({
   secret: ENV.JWT_SECRET,
   algorithms: [SigningAlgorithm],
 });
 
-
 export const authorize = (roles: string[]) => {
-    // Todo: fix any, should be Request
-    return (req: any, res: Response, next: NextFunction) => {
-      if (roles.includes(req.user.role)) {
-        next();
-      } else {
-        return res.status(403).json({ message: 'Unauthorized' });
-      }
-    };
+  // Todo: fix any, should be Request
+  return (req: any, res: Response, next: NextFunction) => {
+    if (roles.includes(req.user.role)) {
+      next();
+    } else {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
   };
-  
+};
