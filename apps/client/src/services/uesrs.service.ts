ef07d5fd7c28692b668b663a8606b2acc5c72a78
Jonathan <jonathandagan12@gmail.com>
2023-05-28 06:49:55 +0300

    update

--- apps/client/src/services/uesrs.service.ts
+++ apps/client/src/services/uesrs.service.ts
@@ -3,9 +3,9 @@ import axios from 'axios';
 export class UsersService {
   private apiUrl = 'http://localhost:3000/api';
 
-  async login(username: string, password: string): Promise<any> {
+  async login(email: string, password: string): Promise<any> {
     const url = `${this.apiUrl}/login`;
-    const body = { username, password };
+    const body = { user: { email, password } };
     const response = await axios.post(url, body);
     localStorage.setItem('token', response.data.token);
     return response.data;
