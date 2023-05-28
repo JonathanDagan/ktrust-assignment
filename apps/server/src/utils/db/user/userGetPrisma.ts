3fab6ebfab79147f9f891492b5daa2b2dcd51744
Jonathan <jonathandagan12@gmail.com>
2023-05-28 04:45:58 +0300

    get user by username

---
+++ apps/server/src/utils/db/user/userGetPrisma.ts
@@ -0,0 +1,12 @@
+import prisma from "../prisma";
+
+export default async function userGetPrisma(username: string) {
+  if (!username) return null;
+  const user = await prisma.user.findUnique({
+    where: { username },
+    include: {
+      authored: true,
+    },
+  });
+  return user;
+}
